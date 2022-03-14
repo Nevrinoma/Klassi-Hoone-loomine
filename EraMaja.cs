42e540fa245bbf4597dc279904fd2486c8bb4281
Opilane TTHK <opilane@tthk.ee>
2022-03-14 11:36:35 +0200

    Add project files.

---
+++ EraMaja.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassi_Hoone_loomine
+{
+    public class EraMaja : Hoone
+    {
+        string staatus;
+        int pind;
+        public EraMaja(int pindala = 50) : base(pindala)
+        {
+
+        }
+
+        public int Suurus
+        {
+            set
+            {
+                pind = value;
+                if (pind < 50)
+                {
+                    staatus = "vaike maja";
+                }
+                else if (pind < 80)
+                {
+                    staatus = "keskmine maja";
+                }
+                else if (pind < 120)
+                {
+                    staatus = "suur maja";
+                }
+                else
+                {
+                    staatus = "liiga suur maja";
+                }
+            }
+            get { return pind; }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassi_Hoone_loomine
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            EraMaja majake = new EraMaja();
+            Inimene mees = new Inimene("Marco");
+            majake.uks = new Uks("must");
+            mees.hoone = majake;
+            mees.NaitaInfo();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
